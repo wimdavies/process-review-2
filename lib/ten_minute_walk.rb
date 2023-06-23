f28af4b5a419612da7781eb3616497ed3104f09c
wimdavies <william.johnston.davies@gmail.com>
2023-06-23 18:08:53 +0100

    Multiline boolean expression for readability

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -1,3 +1,7 @@
 def ten_minute_walk?(walk)
-  (walk.count('n') == 3 && walk.count('s') == 3 && walk.count('e') == 2 && walk.count('w') == 2) || (walk.count('n') == 2 && walk.count('s') == 2 && walk.count('e') == 3 && walk.count('w') == 3)
+  (
+    walk.count('n') == 3 && walk.count('s') == 3 && walk.count('e') == 2 && walk.count('w') == 2
+  ) || (
+    walk.count('n') == 2 && walk.count('s') == 2 && walk.count('e') == 3 && walk.count('w') == 3
+  )
 end
