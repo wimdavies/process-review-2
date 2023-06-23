103ee36eb9ba994281b33789511919670d4294e9
wimdavies <william.johnston.davies@gmail.com>
2023-06-23 17:54:07 +0100

    completed solution for direction, including acceptance criteria test case

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -1,3 +1,3 @@
 def ten_minute_walk?(walk)
-  walk.length == 10 && walk.count('n') == 3 && walk.count('s') == 3 && walk.count('e') == 2 && walk.count('w') == 2
+  walk.length == 10 && (walk.count('n') == 3 && walk.count('s') == 3 && walk.count('e') == 2 && walk.count('w') == 2) || (walk.count('n') == 2 && walk.count('s') == 2 && walk.count('e') == 3 && walk.count('w') == 3)
 end
--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -20,8 +20,16 @@ RSpec.describe 'ten_minute_walk?' do
       expect(ten_minute_walk?(['n', 'e', 's', 'w'])).to eq false
     end
 
-    it 'returns true for a ten-minute walk that is circular' do
+    it 'returns true for a ten-minute walk that is circular (North-South longer)' do
       expect(ten_minute_walk?(['n', 'n', 'n', 'e', 'e', 's', 's', 's', 'w', 'w'])).to eq true
+    end
+
+    it 'returns true for a ten-minute walk that is circular (East-West longer)' do
+      expect(ten_minute_walk?(['n', 'n', '', 'e', 'e', 'e', 's', 's', 'w', 'w', 'w'])).to eq true
+    end
+
+    it 'returns true for the acceptance criteria example' do
+      expect(ten_minute_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq true
     end
   end
 end
